3de10690c91f9b92be0923f57403dee15cb2c650
dundunHa <132465056+dundunHa@users.noreply.github.com>
2024-09-15 03:22:16 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.lixp.dev": dockerHub,
+  "quay.lixp.dev": "https://quay.io",
+  "gcr.lixp.dev": "https://gcr.io",
+  "k8s-gcr.lixp.dev": "https://k8s.gcr.io",
+  "k8s.lixp.dev": "https://registry.k8s.io",
+  "ghcr.lixp.dev": "https://ghcr.io",
+  "cloudsmith.lixp.dev": "https://docker.cloudsmith.io",
+  "ecr.lixp.dev": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.lixp.dev": dockerHub,
 };
 
 function routeByHosts(host) {
